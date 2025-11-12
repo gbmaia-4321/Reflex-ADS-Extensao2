723116c7b451ba08c7ff2760f0123ac55420a4d0
gbmaia-4321 <gbg292526@gmail.com>
2025-11-12 19:57:58 -0300

    cadastro corrigido e js adicionado a cadastro ainda falta coisabagarai

---
+++ src/Js/cadastro.js
@@ -0,0 +1,24 @@
+const botaoProfessor = document.querySelector('.buttonProfessor');
+const botaoAluno = document.querySelector('.buttonAluno');
+
+botaoProfessor.addEventListener('click', function () {
+  // Se já está ativo, desativa
+  if (this.classList.contains('ativo')) {
+    this.classList.remove('ativo');
+  } else {
+    // Ativa este e desativa o outro
+    this.classList.add('ativo');
+    botaoAluno.classList.remove('ativo');
+  }
+});
+
+botaoAluno.addEventListener('click', function () {
+  // Se já está ativo, desativa
+  if (this.classList.contains('ativo')) {
+    this.classList.remove('ativo');
+  } else {
+    // Ativa este e desativa o outro
+    this.classList.add('ativo');
+    botaoProfessor.classList.remove('ativo');
+  }
+});
