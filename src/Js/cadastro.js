c813bf194bd973904a196b8e40104ef1058fa78f
gbmaia-4321 <gbg292526@gmail.com>
2025-11-19 12:32:49 -0300

    fiz uma dash vagaabunda e arrumei os caminhos de ininio para voltar para dashboard aluno, falta de professor, da pra deixaer sem

--- src/Js/cadastro.js
+++ src/Js/cadastro.js
@@ -1,29 +1,37 @@
 const botaoProfessor = document.querySelector('.buttonProfessor');
 const botaoAluno = document.querySelector('.buttonAluno');
+const botaoFinalizar = document.querySelector('#buttonregister');
 
+// Variável para guardar o perfil escolhido
+let perfilSelecionado = null;
+
+// ---- Seleção de perfil ----
 botaoProfessor.addEventListener('click', function () {
-  // Se já está ativo, desativa
-  if (this.classList.contains('ativo')) {
-    this.classList.remove('ativo');
-  } else {
-    // Ativa este e desativa o outro
-    this.classList.add('ativo');
-    botaoAluno.classList.remove('ativo');
-  }
+  perfilSelecionado = "professor";
+  botaoProfessor.classList.add('ativo');
+  botaoAluno.classList.remove('ativo');
 });
 
 botaoAluno.addEventListener('click', function () {
-  // Se já está ativo, desativa
-  if (this.classList.contains('ativo')) {
-    this.classList.remove('ativo');
-  } else {
-    // Ativa este e desativa o outro
-    this.classList.add('ativo');
-    botaoProfessor.classList.remove('ativo');
+  perfilSelecionado = "aluno";
+  botaoAluno.classList.add('ativo');
+  botaoProfessor.classList.remove('ativo');
+});
+
+// ---- Redirecionamento ao finalizar cadastro ----
+botaoFinalizar.addEventListener('click', function (event) {
+
+  // Impede o redirecionamento do <a> caso não tenha escolhido
+  event.preventDefault();
+
+  if (perfilSelecionado === "professor") {
+    window.location.href = "/paginas/professor.html";
   }
-
-  
-
-
+  else if (perfilSelecionado === "aluno") {
+    window.location.href = "/dashboardaluno.html";
+  }
+  else {
+    alert("Por favor, selecione Professor ou Aluno para continuar!");
+  }
 
 });
